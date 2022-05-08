20b7f0414bf69f776ed7b1ce92c7eb3fa9d9c0ca
hggmarks <hggmarks@gmail.com>
2022-05-08 19:31:51 -0300

    update: fixed some errors

--- level-1/level-2/level-3/mux_2to1_tb.v
+++ level-1/level-2/level-3/mux_2to1_tb.v
@@ -1,26 +1,28 @@
 `include "mux_2to1.v"
 
-module mux_2to1_tb;
-    reg OUT;
-    wire SEL;
-    wire D0;
-    wire D1;
+module mux_2to1_tb();
+    wire OUT;
+    reg SEL;
+    reg D0;
+    reg D1;
 
-    mux_2to1_dut(SEL, D0, D1, OUT);
+    mux_2to1 dut(SEL, D0, D1, OUT);
 
     initial begin
 
-        SEL = 1'b0 #10;
+        SEL = 1'b0; #10;
         D0 = 1'b0; D1 = 1'b0; #10;
         D0 = 1'b0; D1 = 1'b1; #10;
         D0 = 1'b1; D1 = 1'b0; #10;
         D0 = 1'b1; D1 = 1'b1; #10;
+        D0 = 1'b0; D1 = 1'b0; #10;
 
-        SEL = 1'b1 #10;
+        SEL = 1'b1; #10;
         D0 = 1'b0; D1 = 1'b0; #10;
         D0 = 1'b0; D1 = 1'b1; #10;
         D0 = 1'b1; D1 = 1'b0; #10;
         D0 = 1'b1; D1 = 1'b1; #10;
+        D0 = 1'b0; D1 = 1'b0; #10;
 
     end
 
